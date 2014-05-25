e63b136bff277302cd3fd285466cf415dc7adb78
Henrik Nygren <nygrenh@gmail.com>
2014-05-25 21:48:22 +0300

    Update test data

--- sql/add-test-data.sql
+++ sql/add-test-data.sql
@@ -1,6 +1,12 @@
 insert into account(name, admin, password)
   values('Smith', TRUE, 'password');
 
+insert into account(name, admin, password)
+  values('Johnson', false, 'gary');
+
+insert into account(name, admin, password)
+  values('Williams', false, 'test');
+
 insert into course(name)
   values('Database Application');
 
@@ -8,7 +14,10 @@ insert into topic(name, course_id, summary, description)
   values('Topic', 1, 'summary', 'something');
 
 insert into project(topic_id, student, hours, grade)
-  values(1, 'Student', 7, 5);
+  values(1, 'Student', 300, 3);
+
+insert into project(topic_id, student, hours, grade)
+    values(1, 'Another Student', 7, 5);
 
 insert into coursestoaccounts(account_id, course_id)
   values(1, 1);
