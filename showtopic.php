cec40cadf2675186f37a0c2689888960ed589724
Henrik Nygren <nygrenh@gmail.com>
2014-06-08 21:00:41 +0300

    Make topic description visible to students

--- showtopic.php
+++ showtopic.php
@@ -2,15 +2,13 @@
   require_once 'lib/common.php';
   require_once 'lib/models/topic.php';
 
-  if (loggedIn()) {
-    $id = (int)$_GET['id'];
-    $topic = Topic::findTopic($id);
-    if($topic==null) {
-      setError('Invalid topic id');
-      header('Location: topics.php?course_id=1');
-    } else {
-      showView("showtopic", 0, array(
-        'topic'=> $topic
-      ));
-    }
+  $id = (int)$_GET['id'];
+  $topic = Topic::findTopic($id);
+  if($topic==null) {
+    setError('Invalid topic id');
+    header('Location: topics.php?course_id=1');
+  } else {
+    showView("showtopic", 0, array(
+      'topic'=> $topic
+    ));
   }
