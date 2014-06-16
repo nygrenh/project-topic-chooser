7860bff55236adbc08feed953b625b9fbf496553
Henrik Nygren <nygrenh@gmail.com>
2014-06-16 20:11:21 +0300

    Implemented validations for coursestoaccounts

--- lib/models/coursestoaccounts.php
+++ lib/models/coursestoaccounts.php
@@ -17,6 +17,15 @@ class CoursesToAccounts {
 
   public function setId($id) {
     $this->id = $id;
+    if ( !is_numeric($id) ) {
+      $this->errors['id'] = "Id should be a number";
+    } else if ( $id <= 0 ) {
+      $this->errors['id'] = "Id should positive";
+    } else if ( !preg_match('/^\d+$/', $id) ) {
+      $this->errors['id'] = "Id should be an integer";
+    } else {
+      unset($this->errors['id']);
+    }
   }
 
   public function getAccountId() {
@@ -25,6 +34,15 @@ class CoursesToAccounts {
 
   public function setAccountId($account_id) {
     $this->account_id = $account_id;
+    if ( !is_numeric($account_id) ) {
+      $this->errors['account_id'] = "Account id should be a number";
+    } else if ( $account_id <= 0 ) {
+      $this->errors['account_id'] = "Account id should positive";
+    } else if ( !preg_match('/^\d+$/', $account_id) ) {
+      $this->errors['account_id'] = "Account id should be an integer";
+    } else {
+      unset($this->errors['account_id']);
+    }
   }
 
   public function getCourseId() {
@@ -33,6 +51,15 @@ class CoursesToAccounts {
 
   public function setCourseId($course_id) {
     $this->course_id = $course_id;
+    if ( !is_numeric($course_id) ) {
+      $this->errors['course_id'] = "Course id should be a number";
+    } else if ( $course_id <= 0 ) {
+      $this->errors['course_id'] = "Course id should positive";
+    } else if ( !preg_match('/^\d+$/', $course_id) ) {
+      $this->errors['course_id'] = "Course id should be an integer";
+    } else {
+      unset($this->errors['course_id']);
+    }
   }
 
 }
